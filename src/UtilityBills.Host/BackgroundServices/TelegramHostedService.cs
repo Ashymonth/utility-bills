21e74e829ca63cb24b4887ccb9cabbb6a093e5a4
kobyakov <kobyakov@segmento.ru>
2025-03-09 21:41:18 +0300

    drop pending update and log tg pooling error

--- src/UtilityBills.Host/BackgroundServices/TelegramHostedService.cs
+++ src/UtilityBills.Host/BackgroundServices/TelegramHostedService.cs
@@ -1,4 +1,6 @@
 using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
 using TelegramBotCommandFramework.Services;
 
 namespace UtilityBills.Host.BackgroundServices;
@@ -7,17 +9,20 @@ public class TelegramHostedService : BackgroundService
 {
     private readonly IServiceProvider _serviceProvider;
     private readonly ITelegramBotClient _botClient;
-    
+
     public TelegramHostedService(IServiceProvider serviceProvider, ITelegramBotClient botClient)
     {
         _serviceProvider = serviceProvider;
         _botClient = botClient;
     }
-    
+
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         var handler = new CommandUpdateHandler(_serviceProvider);
 
-        await _botClient.ReceiveAsync(handler, cancellationToken: stoppingToken);
+        await _botClient.ReceiveAsync((client, update, arg3) => handler.HandleUpdateAsync(client, update, arg3),
+            (_, exception, _) => Console.WriteLine(exception.InnerException?.Message ?? exception.Message),
+            new ReceiverOptions { DropPendingUpdates = true, AllowedUpdates = [UpdateType.Message] },
+            cancellationToken: stoppingToken);
     }
 }
